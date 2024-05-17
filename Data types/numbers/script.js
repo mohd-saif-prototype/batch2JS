903f9ce47417396569af11e4cbc6f4f3129f7bcb
Mohd Saif <786saif321@gmail.com>
2024-05-17 13:22:29 +0530

    Added code for today's class

---
+++ Data types/numbers/script.js
@@ -0,0 +1,21 @@
+// let pi = 3.1428571429;
+
+// console.log("Value of pi:", pi);
+
+// console.log(Math.PI);
+
+// console.log("Rounded value of pi:",Math.round(pi));
+// console.log("Floor value of pi:",Math.floor(pi));
+// console.log("Ceil value of pi:",Math.ceil(pi));
+
+// let myNum = -3.45;
+// console.log("Rounded value of pi:",Math.round(myNum));
+// console.log("Floor value of myNum:",Math.floor(myNum));
+// console.log("Ceil value of myNum:",Math.ceil(myNum));
+// console.log("Absolute value of myNum:",Math.abs(myNum));
+// console.log("Value of myNum with 2 decimal digit:", myNum.toFixed(2));
+
+// console.log("Random number between 0 and 1:", Math.random());
+let otp = Math.round(Math.random() * 1000000);
+console.log("Random number between 0 and 1:", otp);
+
---
+++ Data types/string/script.js
@@ -0,0 +1,7 @@
+let myStr = "Test String";
+
+console.log(`The value of myStr: "${myStr}"`);
+console.log(`The lenght of myStr: `, myStr.length);
+
+console.log(`The last character of myStr: `, myStr[myStr.length - 1]);
+console.log(`The last character of myStr: `, myStr.at(-1));
